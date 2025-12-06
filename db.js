e5842729141677513a5e04ac0a2d11fda762f01e
MacPRO <macpro@MacPROs-MacBook-Pro.local>
2025-12-06 12:06:24 +0500

    change pool

--- db.js
+++ db.js
@@ -4,16 +4,15 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  ssl: false,
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: process.env.NODE_ENV === "production"
-  //   ? { rejectUnauthorized: false }
-  //   : false
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  // host: process.env.PG_HOST,
+  // port: process.env.PG_PORT,
+  // database: process.env.PG_DATABASE
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 })
 
 
